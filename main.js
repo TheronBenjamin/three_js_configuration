485f02dfcfa2f9c8b2c624cb7f1cd4d0184c871c
TheronBenjamin <benjamin.theron.bt@gmail.com>
2022-03-16 00:20:22 +0100

    starting project

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 //import './style.css'
-    // Find the latest version by visiting https://cdn.skypack.dev/three.
+// Find the latest version by visiting https://cdn.skypack.dev/three.
   
     import * as THREE from 'https://cdn.skypack.dev/three@0.126.1';
   
